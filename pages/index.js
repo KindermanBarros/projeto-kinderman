125453e50371793f9cd49ec306c9b49fd2682691
John Kinderman <kindbarros@gmail.com>
2021-03-19 10:08:00 -0400

    Nova home

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+function home(){
+    return (
+        <div>
+            <head><title>Fontes Renováveis </title></head>
+            <header> 
+                <h1>
+                    Projetos
+                </h1>
+                <nav>
+                    <table>
+                        <tr>
+                            <th>Informações</th>
+                        <td>
+                            <a href="/sobre.js">Sobre o projeto</a>
+                        </td>
+                        </tr>
+                    </table>
+                </nav> 
+            </header>
+            <body>
+                <section>
+                    <article>
+                        <header></header>
+                        <p></p>
+                    </article>
+                </section>
+            </body>
+        </div>
+    
+    )
+}
+
+export default home
