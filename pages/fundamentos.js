220a0aa70ed7b93d29ef49defc6be0d1e97876d7
John Kinderman <kindbarros@gmail.com>
2021-03-19 10:30:19 -0400

    Bonito

---
+++ pages/fundamentos.js
@@ -0,0 +1,43 @@
+function home(){
+    return (
+        <div>
+            <head><title>Fontes Renováveis </title></head>
+            <header> 
+                <h1>
+                    Projetos
+                </h1>
+                <nav>
+                    <table id="info" border='1'>
+                        <th>Informações</th>
+                        <tr>
+                        <td>
+                            <a href="/sobre">Sobre o projeto</a>
+                        </td>
+                        </tr>
+                        <tr>
+                        <td>
+                            <a href="/fundamentos">Fundamentos de Energias Renováveis</a>
+                        </td>
+                        </tr>
+                        <tr>
+                        <td>
+                            <a href="/aplicacoes">Aplicações em Energias Renováveis</a>
+                        </td>
+                        </tr>
+                    </table>
+                </nav> 
+            </header>
+            <body>
+                <section>
+                    <article>
+                        <header></header>
+                        <p></p>
+                    </article>
+                </section>
+            </body>
+        </div>
+    
+    )
+}
+
+export default home
